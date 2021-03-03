a0ed320bb7497038f754fb6680f9c1bdb603eb5e
GiovanniMarchetto <73216336+GiovanniMarchetto@users.noreply.github.com>
2021-03-03 09:17:24 +0100

    Refactoring Upload component
    Now Upload component is more indipendent

--- src/components/functions/Upload.vue
+++ src/components/functions/Upload.vue
@@ -2,28 +2,7 @@
   <div>
     <h2>Caricamento file</h2>
     <b-form @submit.prevent="upload">
-      <b-form-group>
-        <legend>File</legend>
-        <!-- <label for="file">File:</label> -->
-        <b-form-file
-          id="file"
-          type="file"
-          name="file"
-          @change.native="trasf64"
-          required
-        />
-
-        <label for="nameFile">Nome del file:</label>
-        <b-form-input
-          id="nameFile"
-          type="text"
-          name="nameFile"
-          placeholder="name of file"
-        />
-
-        <label for="hashtag">Hashtag:</label>
-        <b-form-input id="hashtag" type="text" name="hashtag" placeholder="hashtag" />
-      </b-form-group>
+      <FileInput @change-info="change_home" />
 
       <b-form-group>
         <legend>Dati Consumer</legend>
@@ -32,23 +11,11 @@
           id="usernameCons"
           type="text"
           name="usernameCons"
+          v-model="usernameCons"
           required
           placeholder="username consumer"
         />
-        <label for="nameCons">Nome:</label>
-        <b-form-input
-          id="nameCons"
-          type="text"
-          name="nameCons"
-          placeholder="name consumer"
-        />
-        <label for="emailCons">Email:</label>
-        <b-form-input
-          id="emailCons"
-          type="email"
-          name="emailCons"
-          placeholder="email consumer"
-        />
+        <UserInfo @change-info="change_home" />
       </b-form-group>
 
       <b-button type="submit" variant="success">Upload</b-button>
@@ -57,8 +24,21 @@
 </template>
 
 <script>
+import FileInput from "../input/FileInput";
+import UserInfo from "../input/UserInfo";
+
+import axios from "axios";
+if (localStorage.getItem("jwtToken") != null)
+  axios.defaults.headers["Authorization"] = `Bearer ${localStorage.getItem(
+    "jwtToken"
+  )}`;
+
 export default {
   name: "Upload",
+  components: {
+    FileInput,
+    UserInfo,
+  },
   data() {
     return {
       file: "",
@@ -70,24 +50,58 @@ export default {
     };
   },
   methods: {
-    upload() {
-      const newFile = {
-        file: this.file,
-        nameFile: this.nameFile,
-        hashtag: this.hashtag,
-        usernameCons: this.usernameCons,
-        nameCons: this.nameCons,
-        emailCons: this.emailCons,
-      };
-      this.$emit("upload-evento", newFile);
+    change_home(infos) {
+      const { nameProp, valueProp } = infos;
+      switch (nameProp) {
+        case "file":
+          this.file = valueProp;
+          break;
+        case "nameFile":
+          this.nameFile = valueProp;
+          break;
+        case "hashtag":
+          this.hashtag = valueProp;
+          break;
+        // case "usernameCons":
+        //   this.usernameCons = valueProp;
+        //   break;
+        case "name":
+          this.nameCons = valueProp;
+          break;
+        case "email":
+          this.emailCons = valueProp;
+          break;
+        default:
+          console.log("switch concluso a vuoto");
+          break;
+      }
     },
-
-    trasf64(imgObj) {
-      const reader = new FileReader();
-      reader.onload = (elem) => {
-        this.file = elem.target.result;
-      };
-      reader.readAsDataURL(imgObj.target.files[0]);
+    upload() {
+      axios
+        .post(`${process.env.VUE_APP_APIROOT}/files/upload`, {
+          file: this.file,
+          nameFile: this.nameFile,
+          hashtag: this.hashtag,
+          usernameCons: this.usernameCons,
+          nameCons: this.nameCons,
+          emailCons: this.emailCons,
+        })
+        .then((res) => {
+          if (!res.data.startsWith("ERR")) {
+            this.$emit("upload", res.data);
+            this.file = "";
+            this.nameFile = "";
+            this.hashtag = "";
+            this.usernameCons = "";
+            this.nameCons = "";
+            this.emailCons = "";
+          } else {
+            this.$emit("upload", "ERR - " + res.data);
+          }
+        })
+        .catch((err) => {
+          this.$emit("upload", "ERR(esterno) - " + err);
+        });
     },
   },
 };
