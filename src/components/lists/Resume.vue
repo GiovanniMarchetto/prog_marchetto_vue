622836be0580bfbe020000806bd891925574677d
GiovanniMarchetto <73216336+GiovanniMarchetto@users.noreply.github.com>
2021-03-11 10:59:28 +0100

    Update Resume.vue
    Now in the resume table there are also other informations about uploaders

--- src/components/lists/Resume.vue
+++ src/components/lists/Resume.vue
@@ -7,6 +7,25 @@
     :items="resume"
     :fields="fields"
   >
+    <template #cell(details)="row">
+      <b-button @click="row.toggleDetails">
+        {{ row.detailsShowing ? "Hide" : "Show" }}
+      </b-button>
+    </template>
+
+    <template #row-details="row">
+      <b-card>
+        <b-row class="mb-2">
+          <b-col sm="3" class="text-sm-right"><b>Name:</b></b-col>
+          <b-col>{{ row.item.nameUploader }}</b-col>
+        </b-row>
+
+        <b-row class="mb-2">
+          <b-col sm="3" class="text-sm-right"><b>Email:</b></b-col>
+          <b-col>{{ row.item.emailUploader }}</b-col>
+        </b-row>
+      </b-card>
+    </template>
   </b-table>
 </template>
 
@@ -26,6 +45,7 @@ export default {
           key: "numConsDiversi",
           sortable: true,
         },
+        "details",
       ],
     };
   },
